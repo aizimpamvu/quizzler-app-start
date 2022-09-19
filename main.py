74eaad0b006ae6b361c280a37d0ad330f28fd78d
aizimpamvu <hallyiizza200@gmail.com>
2022-09-19 17:19:37 +0200

    setup ui

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
+from ui import QuizInterface
+
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -11,9 +13,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface()
+#
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- ui.py
+++ ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=200, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
